fac73480d182afcd464e173633567ca2d6d3dd20
vgnshiyer <vgnshiyer@gmail.com>
2024-01-28 19:10:13 -0700

    chore: added text selection color

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ after:bg-white">Navigate through my digital world.</p>
     <html lang="en" className="h-full font-poppins bg-dark">
       <link rel='icon' href='favicon.ico' />
       <body className="mt-6 md:mt-12 lg:mt-24 mb:6 md:mb:12 lg:mb-24">
-        <div className="flex flex-col items-center justify-center mx-auto max-w-9/10 md:max-w-3/4 lg:max-w-2/3">
+        <div className="flex flex-col items-center justify-center mx-auto max-w-9/10 md:max-w-3/4 lg:max-w-2/3 selection:bg-contrast-dark">
           {header}
           {children}
         </div>
